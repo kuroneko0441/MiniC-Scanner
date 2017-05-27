d95dcd459b1a81d6c8114702a43329a915fb5be7
kuroneko0441 <kuroneko0441@gmail.com>
2017-05-28 01:01:41 +0900

    print error & token methods

--- Scanner.cpp
+++ Scanner.cpp
@@ -1,6 +1,32 @@
 #include "Scanner.h"
+#include <iostream>
 
 Scanner::Scanner(std::string fileName):fileName(fileName) {
 	//open file
 	file.open(fileName);
+}
+
+void Scanner::printError(int errorCode) {
+	std::cout << " *** Lexical Error : ";
+	switch(errorCode) {
+	case 0:
+		//default error
+		std::cout << "invalid character\n";
+		break;
+	case 1:
+		//&& error
+		std::cout << "next character must be &\n";
+		break;
+	case 2:
+		//|| error
+		std::cout << "next character must be |\n";
+		break;
+	default: break;
+	}
+}
+
+void Scanner::printToken(Token t) {
+	std::cout << "Token -----> " << t.id << " ";
+	std::cout << "(" << t.number << ", " << t.value << ", " << fileName << ", " << t.lineNum << ", " << t.colNum << ")";
+	std::cout << std::endl;
 }
